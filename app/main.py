50552c8d1bacf419862196358ab868d8cb56f464
Shoaib <shoaib76289@gmail.com>
2024-07-09 23:09:55 +0530

    pypy dev req

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
+from typing import Any
+
 from fastapi import FastAPI
 from app.api.v1.routers import api_router
 
-app = FastAPI()
+from fastapi.responses import JSONResponse
+import ujson
+
+class CustomUJSONResponse(JSONResponse):
+    def render(self, content: Any) -> bytes:
+        return ujson.dumps(
+            content,
+            ensure_ascii=False,
+            escape_forward_slashes=False
+        ).encode("utf-8")
+
+app = FastAPI(deault_response_class=CustomUJSONResponse)
 
 app.include_router(api_router, prefix="/api/v1")
